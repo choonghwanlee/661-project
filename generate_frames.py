eaeb2f425f958cfddd21153dc34460b9a8af3c9e
Jason Lee <choonghwanlee@Jasons-MacBook-Pro.local>
2024-04-05 00:26:09 -0400

    generate_frames

--- generate_frames.py
+++ generate_frames.py
@@ -1,1 +1,26 @@
-## Generate frames from videos
+import cv2
+import os
+
+def extract_frames(video, name):
+    """
+    extract frames from video source  
+    
+    Paramaters
+    ---------
+    video: file (mp4 format)
+        a user-recorded, input video
+    name: string
+        name of the file in string
+    """
+
+    #read video with OpenCV VideoCaptrue
+    cap = cv2.VideoCapture(video)
+    ## video properties
+    ret = True 
+    while ret:
+        ret, frame = cap.read()
+        if ret:
+            msec = cap.get(cv2.CAP_PROP_POS_MSEC)
+            i = int(msec)
+            cv2.imwrite(f"./images/{name}_{i}.jpg", frame)
+    cap.release()
