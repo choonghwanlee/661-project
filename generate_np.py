33b03574157d02443d537a60b5bd59abb67b287d
Cynthia France <cf219@duke.edu>
2024-04-21 02:21:13 -0400

    edit function

--- generate_np.py
+++ generate_np.py
@@ -36,16 +36,13 @@ Returns:
     - String representing the compelte file path
 """
 def images_to_npz(image_filenames, save_path):
-    np_arrays = []
+    np_arrays = {}
 
     for f in image_filenames:
-        np_arrays.append(image_to_np(f"./images/{f}.jpg"))
-    
-    #option 1: dictionary with 1 key whose value is an array of N np arrays
-    np.savez("./{save_path}", np_arrays)
+        np_arrays[f.split("/")[-1].split(".")[0]] = image_to_np(f)
 
-    #option 2: dictionary with N keys whose value is an np array
-    # np.savez("./train_data/unlabelled/test_savez", *np_arrays[:])
+    np.savez_compressed(f"./{save_path}", **np_arrays)
+    print(len(np_arrays))
 
     return save_path+".npz"
 
@@ -73,58 +70,79 @@ def load_npz_data(filepath):
 
     return data
 
-"""
-code to turn images, test images, and both of the test masks to npz files
 
-dir00 = "./images"
-dir0 = "./test_images"
-dir1 = "./masks/iris"
-dir2 = "./masks/pupil"
-np_arrays_train_images = []
+"""
+#code to turn images, test images, and both of the test masks to npz files
+
+imgs = "./images"
+tst_imgs = "./test_images"
+iris_mask = "./masks/iris"
+pupil_mask = "./masks/pupil"
 np_arrays_images = []
+np_arrays_test_images = []
 np_arrays_iris = []
 np_arrays_pupil = []
 
-for filename in os.listdir(dir00):
-    f = os.path.join(dir00, filename)
+for filename in os.listdir(imgs):
+    f = os.path.join(imgs, filename)
     # checking if it is a file
     if os.path.isfile(f) and filename != ".DS_Store":
-        np_arrays_train_images.append(image_to_np(f))
-                         
-np.savez_compressed("./train_data/unlabelled/images", images=np_arrays_train_images)
+        np_arrays_images.append(f)
+images_to_npz(np_arrays_images, "./train_data/unlabelled/images")
 
-for filename in os.listdir(dir0):
-    f = os.path.join(dir0, filename)
+print("/n/n SAVED IMAGES /n/n")
+
+for filename in os.listdir(tst_imgs):
+    f = os.path.join(tst_imgs, filename)
     # checking if it is a file
     if os.path.isfile(f) and filename != ".DS_Store":
-        np_arrays_images.append(image_to_np(f))
-                         
-np.savez_compressed("./train_data/labeled/images", images=np_arrays_images)
+        np_arrays_test_images.append(f)
+images_to_npz(np_arrays_test_images, "./train_data/labeled/images")
 
-for filename in os.listdir(dir1):
-    f = os.path.join(dir1, filename)
+print("/n/n SAVED TEST IMAGES /n/n")
+
+for filename in os.listdir(iris_mask):
+    f = os.path.join(iris_mask, filename)
     # checking if it is a file
     if os.path.isfile(f) and filename != ".DS_Store":
-        np_arrays_iris.append(image_to_np(f))
-                         
-np.savez("./train_data/labeled/iris", np_arrays_iris)
+        np_arrays_iris.append(f)
+images_to_npz(np_arrays_iris, "./train_data/labeled/iris")
 
-print("/n/n SAVED /n/n")
+print("/n/n SAVED IRIS /n/n")
 
-for filename in os.listdir(dir2):
-    f = os.path.join(dir2, filename)
+for filename in os.listdir(pupil_mask):
+    f = os.path.join(pupil_mask, filename)
     # checking if it is a file
     if os.path.isfile(f) and filename != ".DS_Store":
-        np_arrays_pupil.append(image_to_np(f))
+        np_arrays_pupil.append(f)
+images_to_npz(np_arrays_pupil, "./train_data/labeled/pupil")
+
+print("/n/n SAVED PUPIL /n/n")
+
+
+# for filename in os.listdir(dir2):
+#     f = os.path.join(dir00, filename)
+#     # checking if it is a file
+#     if os.path.isfile(f) and filename != ".DS_Store":
+#         filenames.append(f)
+
+#     if len(filenames) > 10:
+#         break
+
+# images_to_npz(filenames, "./train_data/unlabelled/images")
                          
-np.savez_compressed("./train_data/labeled/masks", iris=np_arrays_iris, pupil=np_arrays_pupil)
+# np.savez_compressed("./train_data/labeled/masks", iris=np_arrays_iris, pupil=np_arrays_pupil)
 
-idk = load_npz_data("./train_data/labeled/masks.npz")
-image = Image.fromarray(idk["iris"][0])
-other = load_npz_data("./train_data/labeled/images.npz")
-im = Image.fromarray(other["images"][0])
 
-image.save("./testiris.png")
-im.save("./testimage.png")
 
+
+
+
+# idk = load_npz_data("./train_data/labeled/masks.npz")
+# image = Image.fromarray(idk["iris"][0])
+# other = load_npz_data("./train_data/labeled/images.npz")
+# im = Image.fromarray(other["images"][0])
+
+# image.save("./testiris.png")
+# im.save("./testimage.png")
 """
